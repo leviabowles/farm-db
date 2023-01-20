d8bfed65b7d0483594c1bab37e12b9f145256ecb
Levi Bowles <leviabowles@gmail.com>
2023-01-20 16:56:45 -0600

    working push code with stored creds

--- startAppDev.sh
+++ startAppDev.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
   
 image=farm_dev
+DBUSER=$(keyring get mysql user)
+DBPW=$(keyring get mysql ${DBUSER})
 
+echo "${DBUSER}"
+echo "${DBPW}"
 
-docker run -it -p 8080:8080 --add-host host.docker.internal:host-gateway -e DB="farmdb_dev" -v $(pwd):/var/www --rm ${image} 
+sudo docker run -it -p 8080:8080 --add-host host.docker.internal:host-gateway -e DB="farmdb_dev" -e DBUSER="${DBUSER}" -e DBPW="${DBPW}" -v $(pwd):/var/www --rm ${image} 
