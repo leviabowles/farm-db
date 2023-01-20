e43badeb889bc33ea5e66f7c23695d62a7c9ecf2
Levi Bowles <leviabowles@gmail.com>
2023-01-20 16:30:03 -0600

    Adding first env variables

--- startAppDev.sh
+++ startAppDev.sh
@@ -2,4 +2,5 @@
   
 image=farm_dev
 
-docker run -it -p 8080:8080 --add-host host.docker.internal:host-gateway -v $(pwd):/var/www --rm ${image} 
+
+docker run -it -p 8080:8080 --add-host host.docker.internal:host-gateway -e DB="farmdb_dev" -v $(pwd):/var/www --rm ${image} 
