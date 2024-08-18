843a5b230525f5ad6db42f3f1831012bfeb02d7c
Levi Bowles <leviabowles@gmail.com>
2024-08-17 22:39:16 -0500

    changing mysqldump

--- app_management/db_backup.sh
+++ app_management/db_backup.sh
@@ -1,1 +1,2 @@
-0 3 * * * /usr/bin/mysqldump -u root farmdb > /home/username/db_backups/farmdb-$(date +\%Y\%m\%d).sql
+## change path at install
+0 3 * * * /usr/bin/mysqldump -u root farmdb > /db_backups/farmdb-$(date +\%Y\%m\%d).sql
