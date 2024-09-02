cb898f686025c8f697fea99f4276a4a692c10833
Levi Bowles <leviabowles@gmail.com>
2024-09-01 23:51:03 -0500

    adding function

--- app_management/file_upload_util.py
+++ app_management/file_upload_util.py
@@ -37,6 +37,19 @@ class upload_gdrive:
         self.lastest_file = files[0]
         print(self.lastest_file)
 
+    def print_files(self):
+            # Call the Drive v3 API
+        results = self.service.files().list(
+            pageSize=10, fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+        
+        if not items:
+            print('No files found.')
+        else:
+            print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+
 
 
 '''
