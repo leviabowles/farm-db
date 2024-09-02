4bf3da5357cfbe0cf708d4b3ff3713afcbe92a1a
Levi Bowles <leviabowles@gmail.com>
2024-09-01 23:24:52 -0500

    pushing first round uploader script

--- app_management/file_uploader.py
+++ app_management/file_uploader.py
@@ -1,7 +1,12 @@
 import os
-
+import file_upload_util
+scope = ['https://www.googleapis.com/auth/drive']
+service_account_json_key = 'farm-db-key.json'
 wd = '/home/lb/db_backups/'
+#file_metadata = {'name': wd}
+mime_type = 'text/plain'
 
-files = os.listdir(wd)
-files.sort(reverse = True)
-print(files)
+up_file = file_upload_util.upload_gdrive(scope = scope, key = service_account_json_key)
+up_file.get_latest_file(wd = wd)
+up_file.upload_file(mimetype = mime_type)
+
