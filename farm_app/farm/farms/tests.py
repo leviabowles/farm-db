386e65c4e0f0ef3599b6a516453f73a6aec532c1
Levi Bowles <leviabowles@gmail.com>
2026-07-16 14:47:50 -0500

    testy mctesterson

--- farm_app/farm/farms/tests.py
+++ farm_app/farm/farms/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+
+class FarmViewsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_reporting_page(self):
+        response = self.client.get('/reporting/')
+        self.assertEqual(response.status_code, 200)
